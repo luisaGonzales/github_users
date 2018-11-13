a79081ab8fb9f7d62b7a298c75046874b3710ba7
luisaGonzales <luisagc711@gmail.com>
2018-11-13 04:17:54 -0500

    Splash screen complete, navigate to search

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatefulWidget {
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Github Users'),
+        centerTitle: false,
+      ),
+      body: Center(
+        child: Text('Works'),
+      ),
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacementNamed("/search");
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Center(
+        child: new Image(
+          image: new AssetImage('assets/github-icon.png'),
+          height: 140.0,
+        ),
+      ),
+    );
+  }
+}
+
