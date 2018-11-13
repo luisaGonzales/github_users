87964c432bc3911a899c8383efac0c993f5a04a9
luisaGonzales <luisagc711@gmail.com>
2018-11-13 16:23:07 -0500

    Create reposiory model

---
+++ lib/models/repository_model.dart
@@ -0,0 +1,29 @@
+class Repository {
+  String _name;
+  String _description;
+  int _nPullRequests;
+
+  Repository(
+      this._name,
+      this._description,
+      this._nPullRequests
+      );
+
+  Repository.map(dynamic obj) {
+    this._name = obj['name'];
+    this._description = obj['description'];
+    this._nPullRequests = obj['pullRequests']['totalCount'];
+  }
+
+  String get name => _name;
+  String get description => _description;
+  int get pullRequests => _nPullRequests;
+
+  Map<String, dynamic> toMap() {
+    var map = new Map<String, dynamic>();
+    map["name"] = _name;
+    map["description"] = _description;
+    map["pullRequests"] = _nPullRequests;
+    return map;
+  }
+}
