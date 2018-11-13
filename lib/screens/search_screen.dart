245d48f5f30f40b7bd2b7272d402f099bc18a96d
luisaGonzales <luisagc711@gmail.com>
2018-11-13 05:40:42 -0500

    Test with static data on search list

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -13,9 +14,80 @@ class _SearchScreenState extends State<SearchScreen> {
         title: Text('Github Users'),
         centerTitle: false,
       ),
-      body: Center(
-        child: Text('Works'),
+      body: Query(
+        searchUsers,
+        variables: {'name': "luisaGonzales"},
+        builder: ({
+          bool loading,
+          var data,
+          Exception error,
+        }) {
+          if (error != null) {
+            return Text(error.toString());
+          } else {
+            print(error);
+          }
+
+          if (loading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            print(data);
+          }
+
+          List feedList = data['search']['edges'];
+
+          return ListView.builder(
+            itemCount: feedList.length,
+            itemBuilder: (context, index) {
+              final feedListItems = feedList[index];
+              return new Container(
+                  decoration:
+                  new BoxDecoration(
+                      border: new Border(
+                          bottom: new BorderSide(
+                            color: Colors.grey
+                          )
+                      )
+                  ),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage:
+                    NetworkImage(feedListItems['node']['avatarUrl']),
+                  ),
+                  title: Text(
+                    "${feedListItems['node']['name']}, ${feedListItems['node']['location'] != null ? feedListItems['node']['location'] : 'Not defined'} ",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  subtitle: Text(
+                    feedListItems['node']['login'],
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
 }
+
+String searchUsers = """
+ query SearchUsers{
+  search(query: "Luisa Gonzales", type: USER, first: 10) {
+    edges {
+      node {
+        ... on User {
+          name
+          avatarUrl
+          location
+          login
+        }
+      }
+    }
+  }
+}
+"""
+    .replaceAll('\n', ' ');
