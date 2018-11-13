d439a28497864cee7d1bb47afc907843da6634e0
luisaGonzales <luisagc711@gmail.com>
2018-11-13 17:42:27 -0500

    Navigate to respositories screen with params

--- lib/screens/components/UserTile.dart
+++ lib/screens/components/UserTile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:github_users/models/user_model.dart';
+import 'package:github_users/screens/repositories_screen.dart';
 
 class UserTitle extends StatelessWidget {
   final User _user;
@@ -12,21 +13,24 @@ class UserTitle extends StatelessWidget {
       decoration: new BoxDecoration(
           border: new Border(bottom: new BorderSide(color: Colors.grey))),
       child: new ListTile(
-        leading: CircleAvatar(
-          backgroundImage: NetworkImage(_user.avatarUrl),
-        ),
-        title: Text(
-          "${_user.name}, ${_user.location != null ? _user.location : 'Not defined'} ",
-          style: TextStyle(fontSize: 20.0),
-        ),
-        subtitle: Text(
-          _user.login,
-          style: TextStyle(fontSize: 16.0),
-        ),
-        onTap: () {
-          //Here navigate
-        },
-      ),
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(_user.avatarUrl),
+          ),
+          title: Text(
+            "${_user.name}, ${_user.location != null ? _user.location : 'Not defined'} ",
+            style: TextStyle(fontSize: 20.0),
+          ),
+          subtitle: Text(
+            _user.login,
+            style: TextStyle(fontSize: 16.0),
+          ),
+          onTap: () {
+            Navigator.push(
+                context,
+                new MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        new RepositoriesScreen(name: _user.name, login: _user.login)));
+          }),
     );
   }
 }
