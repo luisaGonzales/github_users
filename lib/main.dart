d84f9953fb36a74a66cdc76a9e5b5f37745fa90c
luisaGonzales <luisagc711@gmail.com>
2018-11-13 04:28:01 -0500

    Add graphQL dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:github_users/utils/routes.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,12 +8,25 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Github Users',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+    ValueNotifier<Client> client = ValueNotifier(
+      Client(
+        endPoint: 'https://api.github.com/graphql',
+        cache: InMemoryCache(),
+        apiToken: '<GITHUB_PERSONAL_ACCESS_TOKEN>',
       ),
-      routes: routes,
+    );
+
+    return GraphqlProvider(
+      client: client,
+      child: CacheProvider(
+          child: MaterialApp(
+            title: 'Github Users',
+            theme: ThemeData(
+              primarySwatch: Colors.blueGrey,
+            ),
+            routes: routes,
+          )
+      ),
     );
   }
 }
